5482ff4c348f94da4641815e1bc564fef66e7f42
Unknown <sophie.candel@etu.unistra.fr>
2019-02-10 19:39:14 +0100

    MAJ

--- dev/includes/footer.php
+++ dev/includes/footer.php
@@ -1,10 +1,10 @@
-
+</main>
 
 <footer class="bg-black">
     <div class="wrapper">
         <p>Projet réalisé dans le cadre du TPE</p>
-        <p>Théo Candel / Weil Lola / Jeanvoine Lisa</p>
-        <p>Lycée Rudloff - année scolaire 2019</p>
+        <p>Lycée Rudloff / année scolaire 2019</p>
+        <p>Candel Théo / Weil Lola / Jeanvoine Lisa</p>
     </div>
 </footer>
 
--- dev/index.php
+++ dev/index.php
@@ -7,18 +7,19 @@
     HELLO WORLD
 </div> -->
 
-<section class="wrapper bg-white m-b-100 p-40">
-    <h1>Comment Starbucks uniformise la consommation de cafés dans le monde et devient un « modèle » économique qui s’impose ? </h1>
+<section class="wrapper bg-white m-b-60 p-40 animated fadeInUp">
+    <h1 class="m-b-40">Comment Starbucks uniformise la consommation de cafés dans le monde et devient un « modèle » économique qui s’impose ? </h1>
 
     <div class="flex-container">
-        <img src="https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" height="450">
+        <img src="https://images.unsplash.com/photo-1525981081253-9859eb266137?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60https://images.unsplash.com/photo-1528732263440-4dd1a18a4cc2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="" height="450">
         <div class="flex-1 m-l-40">
             <p>
-                Starbucks est connu pour être la plus grande société de cafés au monde. L’entreprise a été créé en 1971 à Seattle aux Etats-Unis (depuis Seattle est devenu le siège social de l’entreprise) par Jerry Baldwin, un professeur d'anglais, Zev Siegel, un professeur d'histoire, et l'écrivain Gordon Bowker. Les trois entrepreneurs essaient de lutter contre la société de consommation de masse américaine et produisent des cafés de grande qualité, donc cher que seuls les plus aisés peuvent s’offrir. En 1987 après le rachat de l’entreprise par Howard Schultz, le directeur commercial, la chaîne de cafés prend une autre dimension : en effet, à partir de cette année-là l’homme d’affaire exporte la marque sous le nom de Starbucks Coffee Company à l’étranger. Il va connaître un succès immédiat. Il va même changer le logo qui est devenu une icône de la marque. Il représente une sirène scandinave torse nu (censée évoquer la séduction). Starbucks compte aujourd’hui plus de 29 000 établissements à travers le monde dont 12 000 aux Etats-Unis. 
+            Starbucks est connu pour être la <b>plus grande société de cafés</b> au monde. L’entreprise a été créé en 1971 à Seattle aux Etats-Unis (depuis Seattle est devenu le siège social de l’entreprise) par Jerry Baldwin, un professeur d'anglais, Zev Siegel, un professeur d'histoire, et l'écrivain Gordon Bowker. Les trois entrepreneurs essaient de lutter contre la <b>société de consommation de masse</b> américaine et produisent des cafés de grande qualité, donc cher que seuls les plus aisés peuvent s’offrir. En 1987 après le rachat de l’entreprise par Howard Schultz, le directeur commercial, la chaîne de cafés prend une autre dimension : en effet, à partir de cette année-là l’homme d’affaire exporte la marque sous le nom de <b>Starbucks Coffee Company</b> à l’étranger. Il va connaître un succès immédiat. Il va même changer le logo qui est devenu une icône de la marque. Il représente une sirène scandinave torse nu (censée évoquer la séduction). Starbucks compte aujourd’hui plus de <b>29 000 établissements</b> à travers le monde dont 12 000 aux Etats-Unis. 
             </p>
             <p>
-            Comment Starbucks uniformise la consommation de cafés dans le monde et devient un « modèle » économique qui s’impose ? D’abord nous verrons que c’est une FTN (Firme TransNationale, entreprise qui a son siège social dans un pays d’origine, ici les USA, et qui a créé des filiales dans au moins un autre pays) qui uniformise la consommation à l’échelle mondiale, puis dans un deuxième temps que Starbucks est un « modèle » économique qui s’impose.
+            Comment Starbucks uniformise la consommation de cafés dans le monde et devient un « modèle » économique qui s’impose ? D’abord nous verrons que c’est une FTN (Firme TransNationale, entreprise qui a son siège social dans un pays d’origine, ici les USA, et qui a créé des filiales dans au moins un autre pays) qui uniformise la consommation à l’échelle mondiale, puis dans un deuxième temps que Starbucks est un « modèle » économique qui s’impose. Enfin, nous verrons si Starbucks s'intègre dans une logique de développement durable.
             </p>
+            <button><a href="views/partie1.php">Lire la suite ...</a></button>
         </div>
     </div>
 </section>
